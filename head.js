73a4bf092c28d798d7b6c0dff845786aec164f6c
Jame F <j_frichitthavong@hotmail.com>
2023-05-24 01:58:44 -0400

    something is wrong

---
+++ head.js
@@ -0,0 +1,18 @@
+// Function Implementation
+const assertEqual = function(actual, expected)  {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array)  {
+  console.log(array[0])
+};
+
+//test assertions 
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([2]), 2)
+assertEqual(head([]), undefined)
