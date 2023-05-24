4f4f9cd675ace790d6d6d285c74d44dfdf84a3fd
Jame F <j_frichitthavong@hotmail.com>
2023-05-24 02:00:56 -0400

    2 am club

--- head.js
+++ head.js
@@ -8,11 +8,14 @@ const assertEqual = function(actual, expected)  {
 };
 
 const head = function(array)  {
-  console.log(array[0])
+  if (array.length === 0) {
+    return undefined;
+  }
+  return array[0];
 };
 
 //test assertions 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([2]), 2)
-assertEqual(head([]), undefined)
+assertEqual(head([2]), 2);
+assertEqual(head([]), undefined);
