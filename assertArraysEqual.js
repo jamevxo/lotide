9b5c33c72e7e65ad46bda0c58ca57bf7f42970ae
Jame F <j_frichitthavong@hotmail.com>
2023-05-28 02:36:38 -0400

    happy w assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,32 @@
+// Function Implementation
+const assertEqual = function(actual, expected)  {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Function implementation for eqArrays
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+// Function implementation for assertArraysEqual
+const assertArraysEqual = function(array1, array2) {
+  const result = eqArrays(array1, array2);
+  if (result) {
+    console.log(`✅✅✅ Assertion Passed: [${array1}] === [${array2}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${array1}] !== [${array2}]`);
+  }
+};
