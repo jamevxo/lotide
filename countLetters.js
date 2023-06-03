025ca4f1205f590b1b8c3269d768461a6cb9df3c
Jame F <j_frichitthavong@hotmail.com>
2023-06-03 02:11:14 -0400

    countLetters.js completed

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+// Function Implementation
+const assertEqual = function(actual, expected)  {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const letterCount = {};
+
+  for (const letter of sentence) {
+    if (letter !== ' ') {
+      if (letterCount[letter]) {
+        letterCount[letter] += 1;
+      } else {
+        letterCount[letter] = 1;
+      }
+    }
+  }
+
+  return letterCount;
+};
+
+const result = countLetters("lighthouse in the house");
+console.log(result);
