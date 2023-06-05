d87f6f108cbdaa0166f5c8c403e89ccdab50607f
Jame F <j_frichitthavong@hotmail.com>
2023-06-04 22:54:03 -0400

    Works

---
+++ letterPositions.js
@@ -0,0 +1,56 @@
+const letterPositions = function(sentence) {
+  const results = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    const letter = sentence[i].toLowerCase();
+
+    if (letter !== ' ') {
+      if (!results.hasOwnProperty(letter)) {
+        results[letter] = [];
+      }
+      results[letter].push(i);
+    }
+  }
+
+  return results;
+};
+
+
+
+
+// Function Implementation
+const assertEqual = function(actual, expected)  {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Function implementation for eqArrays
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+// Function implementation for assertArraysEqual
+const assertArraysEqual = function(array1, array2) {
+  const result = eqArrays(array1, array2);
+  if (result) {
+    console.log(`✅✅✅ Assertion Passed: [${array1}] === [${array2}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${array1}] !== [${array2}]`);
+  }
+};
+
+
+assertArraysEqual(letterPositions("hello").e, [1]);
+
