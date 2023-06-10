7d7874d381ba04766a2bb30b7a8813f514d8d4d4
Jame F <j_frichitthavong@hotmail.com>
2023-06-09 22:43:09 -0400

    findKey.js implemented w two passed tests

---
+++ findKey.js
@@ -0,0 +1,42 @@
+// Function Implementation
+const assertEqual = function(actual, expected)  {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKey = function(object, callback) {
+  for (const key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+noma = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma")
+
+expected1 = 'noma'
+assertEqual(noma, expected1)
+
+tooMany = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 4) // => "undefined")
+
+expected2 = 'undefined'
+assertEqual(tooMany, undefined)
