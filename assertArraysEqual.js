bea515876ab8085d46c884a2c0b4b42e1be1ba2c
Jame F <j_frichitthavong@hotmail.com>
2023-06-09 23:58:21 -0400

    refactored assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,24 +1,4 @@
-// Function Implementation
-const assertEqual = function(actual, expected)  {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// Function implementation for eqArrays
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 
 // Function implementation for assertArraysEqual
@@ -30,3 +10,5 @@ const assertArraysEqual = function(array1, array2) {
     console.log(`🛑🛑🛑 Assertion Failed: [${array1}] !== [${array2}]`);
   }
 };
+
+module.exports = assertArraysEqual
