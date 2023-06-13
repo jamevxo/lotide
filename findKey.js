6e76b3159ce9c2b408d4de00cdc7b6db3c550516
Jame F <j_frichitthavong@hotmail.com>
2023-06-13 13:39:25 -0400

    Create findKeyTest, remove test code from findKey

--- findKey.js
+++ findKey.js
@@ -1,13 +1,3 @@
-// Function Implementation
-const assertEqual = function(actual, expected)  {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 const findKey = function(object, callback) {
   for (const key in object) {
     if (callback(object[key])) {
@@ -18,31 +8,3 @@ const findKey = function(object, callback) {
 };
 
 module.exports = findKey;
-
-/* tests
-
-noma = findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma")
-
-expected1 = 'noma'
-assertEqual(noma, expected1)
-
-tooMany = findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 4) // => "undefined")
-
-expected2 = 'undefined'
-assertEqual(tooMany, undefined)
-
-*/
